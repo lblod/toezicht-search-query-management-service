8046121c0a30099e0664de25c809074704430867
Dries Hugaerts <dries.hugaerts@live.be>
2020-07-03 10:38:19 +0200

    Removed enriching of search-qeury form data.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import {getMetaData} from "./lib/enricher";
 
 const FORMS = {
   'ebd65df9-5566-47c2-859a-ceff562881ab': 'share://search-query/config-form.ttl',
-  'e025a601-b50b-4abd-a6de-d0c3b619795c': 'share://search-query/exe-form.ttl'
+  'e025a601-b50b-4abd-a6de-d0c3b619795c': 'share://search-query/filter-form.ttl'
 }
 
 // ENV var.
